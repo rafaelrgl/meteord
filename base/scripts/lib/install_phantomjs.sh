587b401247a0c1f4b764a02c0fadf298ebdcbbd5
Rafael Lima <rafaelrglima@gmail.com>
2018-03-25 15:44:33 -0300

    Fix phantomjs fonts problem when generating pdf is cumming too small

--- base/scripts/lib/install_phantomjs.sh
+++ base/scripts/lib/install_phantomjs.sh
@@ -1,9 +1,9 @@
 # Install PhantomJS
 set -e
 apt-get -y install libfreetype6 libfreetype6-dev fontconfig
-sudo apt-get install -y build-essential g++ flex bison gperf ruby perl
-sudo apt-get install -y libsqlite3-dev libfontconfig1-dev libicu-dev libssl-dev
-sudo apt-get install -y libpng-dev libjpeg-dev python libx11-dev libxext-dev ttf-mscorefonts-installer
+apt-get -y install build-essential g++ flex bison gperf ruby perl
+apt-get -y install libsqlite3-dev libfontconfig1-dev libicu-dev libssl-dev
+apt-get -y install libpng-dev libjpeg-dev python libx11-dev libxext-dev ttf-mscorefonts-installer
 
 ARCH=`uname -m`
 PHANTOMJS_VERSION=2.1.1
