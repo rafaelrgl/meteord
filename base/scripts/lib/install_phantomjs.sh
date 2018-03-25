60ad3901e800e8a1a8b5eb091a1dd82f04780352
Rafael Lima <rafaelrglima@gmail.com>
2018-03-25 15:38:17 -0300

    Organize sequence of package to install phantomjs

--- base/scripts/lib/install_phantomjs.sh
+++ base/scripts/lib/install_phantomjs.sh
@@ -1,8 +1,9 @@
 # Install PhantomJS
 set -e
-sudo apt-get install build-essential g++ flex bison gperf ruby perl \
-sudo apt-get install libsqlite3-dev libfreetype6 libfontconfig1-dev libicu-dev libfreetype6 libssl-dev
-sudo apt-get install libpng-dev libjpeg-dev python libx11-dev libxext-dev ttf-mscorefonts-installer
+apt-get -y install libfreetype6 libfreetype6-dev fontconfig
+sudo apt-get install -y build-essential g++ flex bison gperf ruby perl \
+sudo apt-get install -y libsqlite3-dev libfontconfig1-dev libicu-dev libssl-dev
+sudo apt-get install -y libpng-dev libjpeg-dev python libx11-dev libxext-dev ttf-mscorefonts-installer
 
 ARCH=`uname -m`
 PHANTOMJS_VERSION=2.1.1
