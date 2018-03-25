01560418108bcea437a075eb40ae69192038933b
Rafael Lima <rafaelrglima@gmail.com>
2018-03-25 15:31:51 -0300

    Fix slash at the end of command to install phantomjs

--- base/scripts/lib/install_phantomjs.sh
+++ base/scripts/lib/install_phantomjs.sh
@@ -1,7 +1,7 @@
 # Install PhantomJS
 set -e
 sudo apt-get install build-essential g++ flex bison gperf ruby perl \
-sudo apt-get install libsqlite3-dev libfreetype6 libfontconfig1-dev libicu-dev libfreetype6 libssl-dev \
+sudo apt-get install libsqlite3-dev libfreetype6 libfontconfig1-dev libicu-dev libfreetype6 libssl-dev
 sudo apt-get install libpng-dev libjpeg-dev python libx11-dev libxext-dev ttf-mscorefonts-installer
 
 ARCH=`uname -m`
